4f3299a0a696cb059b3ca96428c6ad450f35391e
Enrico <rico.set06@gmail.com>
2025-05-27 18:28:25 -0300

    Adicionando javascript no  quiz

--- src/js/quiz.js
+++ src/js/quiz.js
@@ -1,2 +1,16 @@
+const quizForm = document.getElementById('quiz-form');
+const quizResult = document.getElementById('quiz-result');
 
+quizForm.addEventListener('submit', e => {
+  e.preventDefault();
+  let score = 0;
+  const formData = new FormData(quizForm);
+  let totalQuestions = 10;
+
+  formData.forEach((value) => {
+    if (value === 'correta') score++;
+  });
+
+  quizResult.innerHTML = `<h3>Você acertou ${score} de ${totalQuestions} perguntas.</h3>`;
+});
 
